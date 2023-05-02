6614cb1adeef15a664c70dc7ff3d30358874930e
soohyuneee <tlstngus95@naver.com>
2023-05-03 03:38:44 +0900

    feat: 제목 추가

---
+++ src/components/Title.tsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+const H2 = styled.h2`
+  margin: 0;
+  text-align: center;
+  display: block;
+
+  @media screen and (min-width: 1040px) {
+    font-size: 2.125rem;
+    font-weight: 700;
+    letter-spacing: -0.018em;
+    line-height: 1.6;
+    margin-bottom: 40px;
+  }
+`;
+
+const Title = () => {
+  return (
+    <H2>
+      국내 모든 임상시험 검색하고
+      <br /> 온라인으로 참여하기
+    </H2>
+  );
+};
+
+export default Title;
