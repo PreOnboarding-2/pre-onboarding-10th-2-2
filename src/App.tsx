041db817354d15dd8d102bab9d73f126589e9bd9
soohyuneee <tlstngus95@naver.com>
2023-05-03 03:37:27 +0900

    design: 글로벌 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
+import GlobalStyle from "./GlobalStyle";
+import styled from "styled-components";
+import Title from "./components/Title";
+import SearchArea from "./components/SearchArea";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 80px 0 160px;
+  max-width: 1040px;
+`;
+
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <GlobalStyle />
+      <Container>
+        <Title />
+        <SearchArea />
+      </Container>
+    </>
+  );
 }
 
 export default App;
