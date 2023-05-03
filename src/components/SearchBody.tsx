b8305737e2098aa6a1793c728dc5ac4486eede08
Hyunmin Choi <hymn9613@gmail.com>
2023-05-03 20:31:19 +0900

    chore: 초기세팅
    - package.json 설정
    - 불필요한 파일 삭제

---
+++ src/components/SearchBody.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SearchBody = () => {
+  return <div>검색창</div>;
+};
+
+export default SearchBody;
---
+++ src/components/SearchHead.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SearchHead = () => {
+  return <div>국내 모든 임상시험 검색하고 온라인으로 참여하기</div>;
+};
+
+export default SearchHead;
