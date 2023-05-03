ab36c3f6c12b1b61cf6057882d3237d39a026a01
Hyunmin Choi <hymn9613@gmail.com>
2023-05-03 21:52:30 +0900

    feat(SearchHead ): 상단 제목 태그 추가 및 스타일 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
+import styled from "styled-components";
+
 import GlobalStyle from "./styles/GlobalStyle";
 import SearchHead from "./components/SearchHead";
 import SearchBody from "./components/SearchBody";
 
-function App() {
+const App = () => {
   return (
     <>
       <GlobalStyle />
-      <SearchHead />
-      <SearchBody />
+      <SearchContainer>
+        <SearchHead />
+        <SearchBody />
+      </SearchContainer>
     </>
   );
-}
+};
+
+const SearchContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 20rem;
+  margin-top: 10rem;
+  background-color: #cae9ff;
+`;
 
 export default App;
--- src/components/SearchHead.tsx
+++ src/components/SearchHead.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import styled from "styled-components";
 
 const SearchHead = () => {
-  return <div>국내 모든 임상시험 검색하고 온라인으로 참여하기</div>;
+  return (
+    <Heading>
+      국내 모든 임상시험 검색하고 <br /> 온라인으로 참여하기
+    </Heading>
+  );
 };
+
+const Heading = styled.div`
+  margin: 40px 0px;
+  text-align: center;
+  font-size: 1.25rem;
+  font-weight: 700;
+  line-height: 1.6;
+  letter-spacing: -0.018em;
+`;
 
 export default SearchHead;
