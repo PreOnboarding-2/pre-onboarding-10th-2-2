a8defa9a9c155b57502de29c5ece704c2f036c59
soohyuneee <tlstngus95@naver.com>
2023-05-03 15:15:06 +0900

    feat: 프록시 설정

--- src/api/API.ts
+++ src/api/API.ts
@@ -1,10 +1,7 @@
 import axios from "axios";
 
-axios.defaults.withCredentials = true;
-
 const API = axios.create({
-  // baseURL: "https://api.clinicaltrialskorea.com/api/v1/search-conditions",
-  baseURL: "http://localhost:3001/list",
+  baseURL: "/api/v1/search-conditions",
 });
 
 export default API;
