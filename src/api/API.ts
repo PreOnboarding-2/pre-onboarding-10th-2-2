c7f81cb53dd30dc6d402963782ad2174438dcf65
soohyuneee <tlstngus95@naver.com>
2023-05-04 16:23:46 +0900

    feat: axios 헤더추가

--- src/api/API.ts
+++ src/api/API.ts
@@ -2,6 +2,9 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL: "/api/v1/search-conditions",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default API;
