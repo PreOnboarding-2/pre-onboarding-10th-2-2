10e2e2801bb5940221cd8c9e4ab5f25cbe7f2191
YejinHwang-D <hyj3463@naver.com>
2023-05-04 15:55:09 +0900

    chore: netlify proxy 설정

--- src/lib/customAxios.ts
+++ src/lib/customAxios.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 import { SERVER } from "../constant/constant";
 
+const PROXY = window.location.hostname === "localhost" ? "" : "/proxy";
+
 const customAxios = axios.create({
-  baseURL: SERVER,
+  baseURL: PROXY + SERVER,
 });
 
 export default customAxios;
