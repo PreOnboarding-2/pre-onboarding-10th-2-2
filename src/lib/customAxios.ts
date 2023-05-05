20fa7c2064899a14167407ca783ee04b53fd24d3
sohan <sohan@student.42seoul.kr>
2023-05-05 11:17:02 +0900

    feat: customAxios response interceptor 추가

--- src/lib/customAxios.ts
+++ src/lib/customAxios.ts
@@ -5,4 +5,12 @@ const customAxios = axios.create({
   baseURL: RESOURCE_PATH,
 });
 
+customAxios.interceptors.response.use(
+  response => response,
+  error => {
+    alert("검색중 오류가 발생했습니다.");
+    return Promise.reject(error);
+  }
+);
+
 export default customAxios;
