aef48dcb68b5e9f5006bc81d665da091d9f40af1
정우재 <dnwo0526@dgsw.hs.kr>
2023-05-05 11:48:53 +0900

    refactor: debounce
    util함수로 로직 분리

--- src/api/fetchSearchSuggestions.tsx
+++ src/api/fetchSearchSuggestions.tsx
@@ -3,6 +3,7 @@ import customAxios from "../lib/customAxios";
 export default async function fetchSearchSuggestions(name: string) {
   try {
     const result = await customAxios.get(`/?name=${name}`);
+    console.info("calling api");
     return result.data;
   } catch (error) {
     if (error instanceof Error) {
--- src/components/search/searchbar/index.tsx
+++ src/components/search/searchbar/index.tsx
@@ -1,85 +1,80 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent } from "react";
 import fetchSearchSuggestions from "../../../api/fetchSearchSuggestions";
 import { BASE_URL, CACHE_STORAGE_NAME, DATE_NAME, RESOURCE_PATH } from "../../../constant";
 import { isCacheExpired } from "../../../utils";
+import { debounce } from "../../../utils/debounce";
 import * as S from "./searchbar.styles";
 import { ISearchBarProps } from "./searchbar.types";
 
-export default function SearchBar(props: ISearchBarProps) {
-  const [debounce, setDebounce] = useState(0);
-
+export default function SearchBar({
+  isVisible,
+  setIsVisible,
+  searchRef,
+  setSearchKeyword,
+  setSearchSuggestions,
+  onClickSubmitSearch,
+  onKeyUpSearchKeyword,
+}: ISearchBarProps) {
   const onChangAutoCompleteSearch = async (event: ChangeEvent<HTMLInputElement>) => {
     const keyword = event.target.value;
+    let fetchData = [];
 
-    if (debounce) window.clearTimeout(debounce);
+    if (keyword) {
+      const cache = await caches.open(CACHE_STORAGE_NAME);
+      const cacheResponse = await cache.match(`${BASE_URL}${RESOURCE_PATH}/?name=${keyword}`);
 
-    const time = window.setTimeout(async () => {
-      let fetchData;
+      if (cacheResponse && !isCacheExpired(cacheResponse)) {
+        fetchData = await cacheResponse.json();
+      } else {
+        fetchData = await fetchSearchSuggestions(keyword);
 
-      if (keyword) {
-        const cache = await caches.open(CACHE_STORAGE_NAME);
-        const cacheResponse = await cache.match(`${BASE_URL}${RESOURCE_PATH}/?name=${keyword}`);
+        let newHeaders = new Headers(fetchData.headers);
+        newHeaders.append(DATE_NAME, new Date().toISOString());
 
-        if (cacheResponse && !isCacheExpired(cacheResponse)) {
-          fetchData = await cacheResponse.json();
-        } else {
-          fetchData = await fetchSearchSuggestions(keyword);
-
-          let newHeaders = new Headers(fetchData.headers);
-          newHeaders.append(DATE_NAME, new Date().toISOString());
-
-          await cache.put(
-            `${BASE_URL}${RESOURCE_PATH}/?name=${keyword}`,
-            new Response(JSON.stringify(fetchData), {
-              headers: newHeaders,
-            })
-          );
-        }
-
-        console.info("calling api");
+        await cache.put(
+          `${BASE_URL}${RESOURCE_PATH}/?name=${keyword}`,
+          new Response(JSON.stringify(fetchData), {
+            headers: newHeaders,
+          })
+        );
       }
+    }
 
-      props.setSearchKeyword(keyword);
-      props.setSearchSuggestions(fetchData.slice(0, 7) || []);
-    }, 300);
-
-    setDebounce(time);
+    setSearchKeyword(keyword);
+    setSearchSuggestions(fetchData.slice(0, 7) || []);
   };
 
   const onFocusAutoCompleteSearch = () => {
-    props.setIsVisible(true);
+    setIsVisible(true);
   };
 
   const onBlurAutoCompleteSearch = () => {
-    if (props.searchRef.current !== null) {
-      props.searchRef.current.value = "";
+    if (searchRef.current !== null) {
+      searchRef.current.value = "";
     }
-    props.setSearchSuggestions([]);
-    props.setIsVisible(false);
+    setSearchSuggestions([]);
+    setIsVisible(false);
   };
 
   return (
-    <S.Container isVisible={props.isVisible}>
+    <S.Container isVisible={isVisible}>
       <S.TextInputWrapper>
         <label htmlFor="search_bar_main"></label>
         <S.TextInput
           type="text"
           id="search_bar_main"
-          placeholder={props.isVisible ? "" : "질환명을 입력해주세요."}
-          ref={props.searchRef}
-          onChange={onChangAutoCompleteSearch}
+          placeholder={isVisible ? "" : "질환명을 입력해주세요."}
+          ref={searchRef}
+          onChange={debounce(onChangAutoCompleteSearch, 300)}
           onFocus={onFocusAutoCompleteSearch}
           onBlur={onBlurAutoCompleteSearch}
           onKeyUp={event =>
-            props.onKeyUpSearchKeyword(
-              event,
-              props.searchRef.current ? props.searchRef.current.value : ""
-            )
+            onKeyUpSearchKeyword(event, searchRef.current ? searchRef.current.value : "")
           }
         />
       </S.TextInputWrapper>
       <S.ButtonWrapper>
-        <S.SearchButton onClick={props.onClickSubmitSearch}>검색</S.SearchButton>
+        <S.SearchButton onClick={onClickSubmitSearch}>검색</S.SearchButton>
       </S.ButtonWrapper>
     </S.Container>
   );
